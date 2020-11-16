f85e5c62a2318d37f181c8ae50cd5e9d7dbf577b
root <root@corbinblock.localdomain>
2020-11-15 20:08:28 -0600

    bash repo 2020-11-15 commit

--- awk_scripts/parsehtml_20201115.sh
+++ awk_scripts/parsehtml_20201115.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#./parsehtml_20201115.sh bookmarks_11_15_20.html
 filename=$1
-awk 'BEGIN { FS = "\"" } ; {print $2}' $filename 
-awk 'BEGIN { FS = "\">" } ; {print $3}' $filename 
+awk 'BEGIN { FS = "\"" } ; {print $2}' $filename | grep http
+#awk 'BEGIN { FS = "\">" } ; {print $3}' $filename | grep " "
